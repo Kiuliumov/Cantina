d5f679bf523148cbd01d0f68c8682410271ab4af
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-21 15:06:10 +0300

    Update device-max-width-css.js

--- scripts/device-max-width-css.js
+++ scripts/device-max-width-css.js
@@ -4,6 +4,6 @@ window.addEventListener('load', getDeviceWidth);
 function getDeviceWidth(){
     const container = document.querySelector('.container')
     const width = window.innerWidth;
-    container.style.width = width + 'px';
+container.style.maxWidth = width + 'px';
     console.log(width)
 }
